7d0b4a2212abdb8ad5bda04542adb425e4312d9e
Ridoy Kumar Adhikary <ridoyofficial134977@gmail.com>
2025-04-25 16:37:41 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed=new Typed(".text",{
+    strings: ["WEB DEVELOPER", "FRONTEND DEVELOPER", "JAVASCRIPT DEVELOPER"],
+    typespeed:500,
+    backspeed:500,
+    backDelay:1000,
+    loop:true
+
+});
+
